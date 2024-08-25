052b11b1b2a91b03ac9a32074faebdbb4430f162
sfK <sfk.sjm@gmail.com>
2024-08-25 13:46:35 +0900

    블로그 글 목록 뷰 구현

---
+++ src/main/java/com/study/blog/controller/BlogViewController.java
@@ -0,0 +1,38 @@
+package com.study.blog.controller;
+
+import com.study.blog.domain.Article;
+import com.study.blog.dto.ArticleListViewResponse;
+import com.study.blog.dto.ArticleViewResponse;
+import com.study.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+}
