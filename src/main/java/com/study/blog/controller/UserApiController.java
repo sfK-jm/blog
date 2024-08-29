244f20f8060f0a9dba8b2585ae891c6b2035e5fd
sfK <sfk.sjm@gmail.com>
2024-08-29 17:38:59 +0900

    로그아웃 추가

--- src/main/java/com/study/blog/controller/UserApiController.java
+++ src/main/java/com/study/blog/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.study.blog.controller;
 
 import com.study.blog.dto.AddUserRequest;
 import com.study.blog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,6 +20,13 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
