dad14c85d196eb8aa156126d88c3cbefe002e668
sfK <sfk.sjm@gmail.com>
2024-09-02 17:05:17 +0900

    토큰 서비스 추가

---
+++ src/main/java/com/study/blog/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.study.blog.service;
+
+import com.study.blog.domain.RefreshToken;
+import com.study.blog.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
